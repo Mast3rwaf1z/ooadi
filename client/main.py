6f5cb79f3250e870d30bb97e905f800dddc5f880
breadzilla <rapovan@gmail.com>
2022-11-18 14:41:29 +0100

    Updated gui

--- client/main.py
+++ client/main.py
@@ -21,7 +21,7 @@ class Account:
 
 
 def main():
-    acc = Account("Bob", "1234")
+    acc = Account("Jack", "1234")
 
     window = Tk()
     window.title("Totally legit sensor app")
@@ -53,15 +53,15 @@ def main():
         password = passwordTextBox.get()
         print(username)
         print(password)
-        
+
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect(('localhost', 8000))
         s.send(f'login {username} {password}\n'.encode('utf-8'))
 
-        recv = s.recv(1024).decode("utf-8")
-        if recv == "failed":
-            print("Failed to log in")
-            exit(0)
+        #recv = s.recv(1024).decode("utf-8")
+        #if recv == "failed":
+            #print("Failed to log in")
+            #exit(0)
 
         if username == acc.getUsername() and password == acc.getPassword():
             myLabel.configure(text="Log in successful", fg='green', bg='#84A9C0')
@@ -100,11 +100,79 @@ def MainMenu(window):
         frameSShow = Frame(frameShow, width=500, height=350)
         frameSShow.place(x=50, y=150)
 
-        showLabel = Label(frameShow, text="Select the sensor you want to show", bg='#84A9C0')
-        showLabel.place(x=50, y=50)
+        serverListLabel = Label(frameShow, text="Select the sensor you want to:", bg='#84A9C0')
+        serverListLabel.place(x=50, y=50)
+
+        showLabel = Label(frameShow, text="Show", bg='#84A9C0')
+        showLabel.place(x=50, y=125)
+
+        hideLabel = Label(frameShow, text="Hide", bg='#84A9C0')
+        hideLabel.place(x=300, y=125)
 
         exitButton = Button(frameShow, text="Exit", bg='#84A9C0', command=frameShow.destroy)
         exitButton.place(x=50, y=550)
+
+        #list box
+        showListBox = Listbox(frameSShow, width=250, height=350)
+        showListBox.place(x=0, y=0)
+
+        hideListBox = Listbox(frameSShow, width=250, height=350)
+        hideListBox.place(x=250, y=0)
+
+        errorLabel = Label(frameShow, text='', bg='#84A9C0')
+        errorLabel.place(x=300, y=550)
+
+        def hideIt():
+            if len(showListBox.curselection()) != 0:
+                x = showListBox.get(showListBox.curselection())
+                hideListBox.insert(END, x)
+                errorLabel.configure(text='')
+                showListBox.delete(ANCHOR)
+            else:
+                errorLabel.configure(text="Select an element first")
+
+        toHideButton = Button(frameShow, text="Hide", command=hideIt, bg='#84A9C0')
+        toHideButton.place(x=400, y=500)
+
+        def showIt():
+            if len(hideListBox.curselection()) != 0:
+                x = hideListBox.get(hideListBox.curselection())
+                showListBox.insert(END, x)
+                errorLabel.configure(text='')
+                hideListBox.delete(ANCHOR)
+            else:
+                errorLabel.configure(text="Select an element first")
+
+        toShowButton = Button(frameShow, text="Show", command=showIt, bg='#84A9C0')
+        toShowButton.place(x=150, y=500)
+
+        def saveIt():
+            errorLabel.configure(text="Saved", fg='green', bg='#84A9C0')
+            saveButton['state'] = DISABLED
+            exitButton['state'] = DISABLED
+            toShowButton['state'] = DISABLED
+            toHideButton['state'] = DISABLED
+            window.after(2000, frameShow.destroy)
+
+            #Saves the progress
+
+        saveButton = Button(frameShow, text="Save", command=saveIt, bg='#84A9C0')
+        saveButton.place(x=500, y=550)
+
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect(('localhost', 8000))
+        s.send('getids\n'.encode('utf-8'))
+
+        #add item to list box
+        showListBox.insert(END, "an item")
+        showListBox.insert(END, "an  different item")
+
+        #Add list of items
+        a_list = ["one", "two", "three"]
+
+        for item in a_list:
+            showListBox.insert(END, item)
+
 
     showButton = Button(frameMenu, text="Show", bg='#84A9C0', command=showClick)
     showButton.place(x=100, y=100)
